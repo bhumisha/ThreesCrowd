e7831ed5f14721106b67e65d78fc096a8ca5df7d
Bhumisha Dave <bhumishadave@Bhumishas-MBP.hsd1.ca.comcast.net>
2021-01-25 01:21:37 -0800

    Update LocalStorage details - Win,Loose,Game counters Also added Player Info Model

--- assets/js/game.js
+++ assets/js/game.js
@@ -46,12 +46,25 @@ $(document).on("click",".image",function(event){
 })
 
 var play = function(event) {
+    $("#main").text('');
+    $('#playerName-modal').modal('show');
+    $('#GoModalButtonEl').on("click", savePlayersInfoToLocalStorage);
     event.preventDefault();
     var gameDiv = $("<div>");
+
     var playerDiv = $("<div>").addClass("playerDiv");
-    var computerDiv = $("<div>").addClass("computerDiv");
-    computerDiv.attr("id","computerDiv");
+    var playerDivHeadingEl = $("<h4>").addClass("ui yellow header");
     playerDiv.attr("id","playerDiv");
+    
+    playerDivHeadingEl.attr("id","playerDivHeadingEl");
+    playerDiv.append(playerDivHeadingEl);
+
+    var computerDiv = $("<div>").addClass("computerDiv");
+    var computerDivHeadingEl = $("<h4>").addClass("ui yellow header");;
+    computerDiv.attr("id","computerDiv");
+    computerDivHeadingEl.text("Computer");
+    computerDiv.append(computerDivHeadingEl)
+   
     for(var i=1;i<=5;i++){
         var imgPlayerEl = $("<img>");
         imgPlayerEl.attr("id","P"+i);
@@ -80,6 +93,37 @@ var play = function(event) {
 
 $("#playGame").on("click", play);
 
+
+function savePlayersInfoToLocalStorage(){
+    debugger;
+    var playersList = JSON.parse(localStorage.getItem("playersList")) || [];
+        var playerName = $("#playerName").val().trim();
+        $("#playerDivHeadingEl").text(playerName);
+        var isPlayerExist = false;
+        if(playerName!=""){
+            for(var i=0;i<playersList.length;i++)
+            {
+                var playerDetail = playersList[i];
+                if(playerName === playerDetail.name){
+                    isPlayerExist = true;
+                }
+            }
+            if(!isPlayerExist){
+                var player = { name : playerName, 
+                               win : 0,
+                               loose : 0,
+                               game : 1}
+                playersList.push(player);
+                
+                localStorage.setItem('playersList',JSON.stringify(playersList));
+                
+               // $("#playerName").val('');
+                
+            }
+        }
+}
+
+
 function loadCardToPlayerPanel(data,selectedImageEl){
     if(data){
         var playerClickedImageEl = $("#P"+selectedImageEl);
@@ -91,7 +135,8 @@ function loadCardToPlayerPanel(data,selectedImageEl){
         
     }
     if(checkWinner("player")){
-        loadWinnerDiv("player")
+        loadWinnerDiv($("#playerDivHeadingEl").text());
+        updatePlayersResultToLocalStorage($("#playerDivHeadingEl").text());
     }
 
 }
@@ -105,8 +150,7 @@ function loadCardToComputerPanel(data,selectedImageEl){
         computerImageEl.attr("cardCode",data.cards[1].code);
         computerImageEl.attr("cardSuit",data.cards[1].suit);
         computerImageEl.attr("cardValue",data.cards[1].value);
-            
-       
+             
     }
     if(checkWinner("computer")){
         loadWinnerDiv("computer")
@@ -116,72 +160,41 @@ function loadCardToComputerPanel(data,selectedImageEl){
 
 function checkWinner(checkWinnerFor){
     
-   
 }
-
 function loadWinnerDiv(winner){
     
 }
-function storeResultToLocalStorage(){
 
+function updatePlayersResultToLocalStorage(winnerName){
+    debugger;
+    var playersList = JSON.parse(localStorage.getItem("playersList")) || [];
+        
+        if(winnerName!=""){
+            for(var i=0;i<playersList.length;i++)
+            {
+                var playerDetail = playersList[i];
+                if(winnerName === playerDetail.name){
+                    playerDetail.win++;
+                    playerDetail.game++;
+                    playerDetail.loose = playerDetail.game - playerDetail.win;
+                    playersList[i] = playerDetail;
+                }
+            }      
+            localStorage.setItem('playersList',JSON.stringify(playersList));
+                
+            $("#playerName").val('');
+        }
 }
 
 function loadResultFromLocalStorage(){
     
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 $('#gameRules').on("click", function(event) {
     $('#rules-modal').modal('show');
     $('#playGameModal').on("click", play);
 });
+
+
+
+
+
